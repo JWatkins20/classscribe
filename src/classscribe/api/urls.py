880cb4f4f7c2346bf509aaebbe35b8923778db72
JWatkins20 <38019485+JWatkins20@users.noreply.github.com>
2019-10-11 01:36:35 -0400

    rest framework with registration and login

---
+++ src/classscribe/api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('registration/', include('rest_auth.registration.urls')),
+	path('', include('rest_auth.urls'))
+]
