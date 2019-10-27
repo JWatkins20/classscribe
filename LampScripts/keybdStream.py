537abfbe6b24aa8f22915e45db4f6cfc272692b0
Rahat Maini <rahatmaini@live.com>
2019-10-27 17:57:22 -0400

    Create keybdStream.py

---
+++ LampScripts/keybdStream.py
@@ -0,0 +1,16 @@
+import string
+
+from evdev import InputDevice
+from select import select
+idNum=[]
+keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
+dev = InputDevice('/dev/input/by-id/usb-HXGCoLtd_27db-event-kbd')
+
+def waitingForID():
+        while True:
+                r,w,x = select([dev], [], [])
+                for event in dev.read():
+                        if event.type==1 and event.value==1:
+                                idNum.append( keys[ event.code ] )
+                        if ('X' in idNum):
+                                return (idNum)
