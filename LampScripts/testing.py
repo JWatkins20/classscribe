f69eaa59a3a33d91b68c5799451e700e71722d68
Rahat Maini <rahatmaini@live.com>
2019-10-27 18:00:18 -0400

    Update testing.py

--- LampScripts/testing.py
+++ LampScripts/testing.py
@@ -2,9 +2,11 @@ import unittest
 import main
 
 class TestStringMethods(unittest.TestCase):
-
+    
+    # Tapping Ben's ID should return this
     def test_Ben_ID(self):
         self.assertEqual("['2', '1', '5', '3', '4', '4', '3', '2', '1', '8', 'X']",main.waitingForID())
 
 if __name__ == '__main__':
     unittest.main()
+    
