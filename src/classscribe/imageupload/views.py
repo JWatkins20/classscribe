344704b95388797e84fffb21ddb40eeaa5119024
hank-weber <hpw3nr@virginia.edu>
2019-10-27 13:33:56 -0400

    remove unnecessary print

--- src/classscribe/imageupload/views.py
+++ src/classscribe/imageupload/views.py
@@ -25,7 +25,6 @@ class FileUploadView(APIView):
 
 def scan_view(request):
     obj = File.objects.filter(remark="hank", class_name="test2")
-    print(obj.count(), "matches.")
     return render_to_response('image_test.html', {"images": obj})
 
 # Use the following code to POST from python
