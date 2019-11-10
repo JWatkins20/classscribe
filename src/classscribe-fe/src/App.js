4e0798a7bca0ad1c69efe2dc3b4025e85d524df8
hank-weber <hpw3nr@virginia.edu>
2019-11-10 10:52:21 -0500

    download all notebooks for hardcoded user

--- src/classscribe-fe/src/App.js
+++ src/classscribe-fe/src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import CourseForm from "./components/customAdmin/index";
 import ImageCarousel from "./components/ImageUpload/index";
+import NotebookDownload from "./components/ImageUpload/download";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/notebook-carousel" exact component={ImageCarousel} />
         <Route path="/create-course" exact component={CourseForm} />
+        <Route path="/download-notebooks" exact component={NotebookDownload} />
+        <Route path="/notebook-carousel" exact component={ImageCarousel} />
       </Router>
     );
   }
