00f6e8b0fd652d2bd9636ea8759201e8fc23e8c3
hank-weber <hpw3nr@virginia.edu>
2019-11-10 23:01:51 -0500

    hpw3nr sprint 4 unit tests

--- src/classscribe/custom_admin/tests.py
+++ src/classscribe/custom_admin/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Course
 
-# Create your tests here.
+class CourseTests(TestCase):
+
+    def setUp(self):
+        Course.objects.create(
+            room="testRoom",
+            time="testTime",
+            name="testName",
+            building="testBuilding",
+            professorID="testProfessorID",
+            lamp_serial="testLamp_serial"
+        )
+        Course.objects.create(
+            room="testRoom1",
+            time="testTime",
+            name="test Name",
+            building="testBuilding",
+            professorID="testProfessorID",
+            lamp_serial="testLamp_serial"
+        )
+
+    def test_finds_course(self):
+        found_course = None
+        found_course = Course.objects.get(
+            room="testRoom",
+            time="testTime",
+            name="testName",
+            building="testBuilding",
+            professorID="testProfessorID",
+            lamp_serial="testLamp_serial"
+        )
+        self.assertNotEqual(found_course, None)
+
+    def test_edit_course_finds_course(self):
+        c = Client()
+        response = c.get('/courses/edit/testName/testBuilding/testRoom/testTime')
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_course_finds_course_with_space(self):
+        c = Client()
+        response = c.get('/courses/edit/test Name/testBuilding/testRoom1/testTime')
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_course_fails_to_find_course(self):
+        c = Client()
+        response = c.get('/courses/edit/testName/testBuilding/testRoom1/testTime')
+        self.assertEqual(response.status_code, 400)
+
+    def test_edit_course_succeeds_on_empty_param(self):
+        c = Client()
+        response = c.get('/courses/edit/testName//testRoom1/testTime')
+        self.assertEqual(response.status_code, 200)
