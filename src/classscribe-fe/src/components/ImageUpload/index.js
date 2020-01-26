c296abfc1c739bc7bdee02ac3a74463b2398b837
bfb3ab <43013544+bfb3ab@users.noreply.github.com>
2020-01-26 14:32:39 -0500

    removed redundant import statements

--- src/classscribe-fe/src/components/ImageUpload/index.js
+++ src/classscribe-fe/src/components/ImageUpload/index.js
@@ -12,10 +12,8 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import CardContent from '@material-ui/core/CardContent';
 import EditIcon from '@material-ui/icons/Edit';
-import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card';
 import Switch from '@material-ui/core/Switch';
 import borderColor  from '@material-ui/system/borders';
