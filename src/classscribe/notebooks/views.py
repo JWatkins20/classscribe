02359caf4ad0d4b3c57519e7ac477e4be9d0ddad
bfb3ab <43013544+bfb3ab@users.noreply.github.com>
2020-02-23 11:01:55 -0500

    travis fix

--- src/classscribe/notebooks/views.py
+++ src/classscribe/notebooks/views.py
@@ -45,14 +45,12 @@ def notebook_page_view(request, pk):
 
 @api_view(["GET"])
 def retrieve_public_notebooks(request, pk, class_name):
-	
-    obj = Notebook.objects.filter(~Q(owner__pk__exact=pk) & Q(Private=False) & Q(class_name=class_name.replace("%20", " ")))# finds pages with remark matching parameter
-    objs = []
-    for book in obj:
-        objs.append(NotebookSerializer(book).data)
-    # if not objs:
-    #     return Response(status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_200_OK, data={"data": objs})
+	user = User.objects.get(pk=pk)
+	obj = Notebook.objects.filter(~Q(owner__pk__exact=pk) & ~Q(FavoritedBy=user) & Q(Private=False) & Q(class_name=class_name.replace("%20", " ")))# finds pages with remark matching parameter
+	objs = []
+	for book in obj:
+    		objs.append(NotebookSerializer(book).data)
+	return Response(status=status.HTTP_200_OK, data={"data": objs})
 
 
 '''
@@ -106,7 +104,6 @@ class PageCreateView(APIView):
 				page.save()
 				return Response({"key": page.pk}, status=status.HTTP_201_CREATED)
 			except:
-				print(serializer.save())
 				return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -150,23 +147,21 @@ def add_file_view(request):
 	else:
 		return Response(status=status.HTTP_400_BAD_REQUEST)
 
-# @api_view(["POST"])
-# def split_page(request):
-# 	data = request.data
+@api_view(["POST"])
+def split_page(request):
+	data = request.data
 
-# 	notebook = Notebook.objects.get(pk=data["notebook_pk"])
-# 	page1 = Page.objects.get(pk=data["page1_pk"])
-# 	page2 = Page.objects.get(pk=data["page2_pk"])
-# 	image = Page.objects.get(pk=data["image_pk"])
+	notebook = Notebook.objects.get(pk=data["notebook_pk"])
+	page1 = Page.objects.get(pk=data["page1_pk"])
+	page2 = Page.objects.get(pk=data["page2_pk"])
+	image = Page.objects.get(pk=data["image_pk"])
 
 
 @api_view(["POST"])
 def edit_notebook_view(request):
     data = request.data
-    #print(data.keys())
     notebook = Notebook.objects.get(pk=data["pk"])
     notebook.name = data["name"]
-    #notebook.private = data["private"]
     try:
         notebook.save()
         return Response(status=status.HTTP_200_OK, data={})
@@ -201,21 +196,38 @@ def add_audio_and_transcript_view(request):
 	else:
 		return Response(status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(["POST"])
+def favorite_notebook_view(request):
+	data = request.data
+	#notebooks = Notebook.objects.filter(pk in data["book_pks"])
+	user = User.objects.get(pk=data["user_pk"])
+	number_added = 0
+	try:
+		for n in data["books_pks"]:
+			notebook = Notebook.objects.get(pk=n)
+			if(user not in notebook.favoritedBy.all()):
+				notebook.favoritedBy.add(user)
+			else:
+				return Response(status.HTTP_400_BAD_REQUEST, data={'msg': 'Could not add to favorited books'})
+		return Response(status=status.HTTP_201_CREATED)
+	except Exception as e:
+		return Response(status.HTTP_400_BAD_REQUEST, data={'msg': 'Could not add to favorited books'})
 
 
-# class ProcessingView(APIView):
-# 	def get(self, request):
-# 		notebook1 = Notebook.objects.create(Private=False, class_name="Capstone Practicum", name="bfb3ab_11/4/2019_notes")
-# 		file1 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test1", class_name="Practicum", page_num="1")
-# 		file2 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test2", class_name="Something else", page_num="2")
-# 		file3 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test3", class_name="Practicum", page_num="3")
-# 		file4 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test4", class_name="Something else", page_num="4")
-# 		page1 = Page.objects.create(name="Page name")
-# 		page1.snapshots.add(file1)
-# 		page1.snapshots.add(file2)
-# 		page1.snapshots.add(file3)
-# 		page1.notebook = notebook1
-# 		return Response()
+
+class ProcessingView(APIView):
+	def get(self, request):
+		notebook1 = Notebook.objects.create(Private=False, class_name="Capstone Practicum", name="bfb3ab_11/4/2019_notes")
+		file1 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test1", class_name="Practicum", page_num="1")
+		file2 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test2", class_name="Something else", page_num="2")
+		file3 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test3", class_name="Practicum", page_num="3")
+		file4 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test4", class_name="Something else", page_num="4")
+		page1 = Page.objects.create(name="Page name")
+		page1.snapshots.add(file1)
+		page1.snapshots.add(file2)
+		page1.snapshots.add(file3)
+		page1.notebook = notebook1
+		return Response()
 
 
 @api_view(["GET"])
