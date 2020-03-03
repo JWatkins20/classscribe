f52c9f4b93c94af77180371e0f640df6e293c268
bfb3ab <43013544+bfb3ab@users.noreply.github.com>
2020-03-02 19:47:56 -0500

    Update views.py
    Up to date with master

--- src/classscribe/notebooks/views.py
+++ src/classscribe/notebooks/views.py
@@ -242,19 +242,19 @@ def unfavorite_notebook_view(request):
 		return Response(status=status.HTTP_400_BAD_REQUEST, data={'msg': str(e)})
 
 
-# class ProcessingView(APIView):
-# 	def get(self, request):
-# 		notebook1 = Notebook.objects.create(Private=False, class_name="Capstone Practicum", name="bfb3ab_11/4/2019_notes")
-# 		file1 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test1", class_name="Practicum", page_num="1")
-# 		file2 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test2", class_name="Something else", page_num="2")
-# 		file3 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test3", class_name="Practicum", page_num="3")
-# 		file4 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test4", class_name="Something else", page_num="4")
-# 		page1 = Page.objects.create(name="Page name")
-# 		page1.snapshots.add(file1)
-# 		page1.snapshots.add(file2)
-# 		page1.snapshots.add(file3)
-# 		page1.notebook = notebook1
-# 		return Response()
+class ProcessingView(APIView):
+	def get(self, request):
+		notebook1 = Notebook.objects.create(Private=False, class_name="Capstone Practicum", name="bfb3ab_11/4/2019_notes")
+		file1 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test1", class_name="Practicum", page_num="1")
+		file2 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test2", class_name="Something else", page_num="2")
+		file3 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test3", class_name="Practicum", page_num="3")
+		file4 = File.objects.create(file=SimpleUploadedFile("test.jpg", b"hello world"), remark="test4", class_name="Something else", page_num="4")
+		page1 = Page.objects.create(name="Page name")
+		page1.snapshots.add(file1)
+		page1.snapshots.add(file2)
+		page1.snapshots.add(file3)
+		page1.notebook = notebook1
+		return Response()
 
 
 @api_view(["GET"])
