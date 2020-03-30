70ce3c7db998e9275ac4b2ed476476cdc04d9a46
bfb3ab <43013544+bfb3ab@users.noreply.github.com>
2020-03-30 13:34:55 -0400

    Update index.js

--- src/classscribe-fe/src/components/ImageUpload/index.js
+++ src/classscribe-fe/src/components/ImageUpload/index.js
@@ -408,7 +408,7 @@ async loadNotes()
       snap_times = this.state.items[this.state.notebook].pages[this.state.page].snapshots.map(function(x){
         let snaptime = new Date(x.timestamp)
         console.log(t.parseDate(snaptime))
-        return t.parseDate(snaptime))
+        return t.parseDate(snaptime)
       })
     }
     let audio_duration = Math.floor(this.state.duration)
